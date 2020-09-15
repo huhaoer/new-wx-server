d8d1b6e4ad00e22ccdd7cf8419edaaa6d4b5913f
layoung <184592775@qq.com>
2020-09-15 21:45:00 +0800

    finish'

--- app.js
+++ app.js
@@ -14,25 +14,33 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/auth', (req, res) => {
-    console.log(req.query,'????');
+    console.log(req.query, '????');
+    /**
+     *   signature: 'c9cb9b15802a0e3aecf4ffb7d65e608c230e6bc4',
+        echostr: '6136597064511585534',
+        timestamp: '1600177188',
+        nonce: '1593833679'
+
+     */
     const {
-      signature,
-      timestamp,
-      nonce,
-      echostr
+        signature,
+        timestamp,
+        nonce,
+        echostr
     } = req.query; //解构微信服务器传递的数据
     const sortArray = [timestamp, nonce, TOKEN];
     sortArray.sort(); //字典排序
     const sortStr = sortArray.join(''); //转变为字符串
     const sha1Str = sha1(sortStr); //对排序的字符串进行sha1加密
+    console.log(signature,sha1Str,signature === sha1Str);
     if (signature === sha1Str) {
-      // 设置响应头 不然默认返回的时html
-      res.set('Content-Type', 'text/plain')
-      res.send(echostr)
+        // 设置响应头 不然默认返回的时html
+        res.set('Content-Type', 'text/plain')
+        res.send(echostr)
     } else {
-      res.send('Error! No Auth')
+        res.send('Error! No Auth')
     }
-  })
-  
+})
+
 
 module.exports = app;
