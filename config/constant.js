5d7110882807e70470fec121879982ff8a45ef26
layoung <184592775@qq.com>
2020-09-15 20:45:23 +0800

    first commit

---
+++ config/constant.js
@@ -0,0 +1,6 @@
+/**
+ * 常量配置
+ */
+module.exports = {
+    TOKEN: 'wxauth',//服务器token
+}
---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+const { TOKEN } = require('../config/constant')
+
+// 验证服务器权限 GET 微信服务器向layoung.club/auth发送请求携带参数
+router.get('/auth', (req, res) => {
+  const {
+    signature,
+    timestamp,
+    nonce,
+    echostr
+  } = req.query; //解构微信服务器传递的数据
+  const sortArray = [timestamp, nonce, TOKEN];
+  sortArray.sort(); //字典排序
+  const sortStr = sortArray.join(''); //转变为字符串
+  const sha1Str = sha1(sortStr); //对排序的字符串进行sha1加密
+  if (signature === sha1Str) {
+    // 设置响应头 不然默认返回的时html
+    res.set('Content-Type', 'text/plain')
+    res.send(echostr)
+  } else {
+    res.send('Error! No Auth')
+  }
+})
+
+module.exports = router;
