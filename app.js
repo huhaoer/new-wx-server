cf273a658570025aaeca2a48ed192b3d1b30afae
layoung <184592775@qq.com>
2020-09-15 21:40:25 +0800

    'submit'

--- app.js
+++ app.js
@@ -13,6 +13,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(indexRouter);
+app.get('/auth', (req, res) => {
+    console.log(req.query,'????');
+    const {
+      signature,
+      timestamp,
+      nonce,
+      echostr
+    } = req.query; //解构微信服务器传递的数据
+    const sortArray = [timestamp, nonce, TOKEN];
+    sortArray.sort(); //字典排序
+    const sortStr = sortArray.join(''); //转变为字符串
+    const sha1Str = sha1(sortStr); //对排序的字符串进行sha1加密
+    if (signature === sha1Str) {
+      // 设置响应头 不然默认返回的时html
+      res.set('Content-Type', 'text/plain')
+      res.send(echostr)
+    } else {
+      res.send('Error! No Auth')
+    }
+  })
+  
 
 module.exports = app;
