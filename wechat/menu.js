ac0e1ec6b54fd1aba03e0def453b737f4867d5a7
layoung <184592775@qq.com>
2020-09-16 12:38:30 +0800

    'menu'

--- wechat/menu.js
+++ wechat/menu.js
@@ -4,31 +4,59 @@
 const { BASEURL } = require('../config/constant');
 
 module.exports = {
+  // "button": [
+  //   {
+  //     "type": "view",
+  //     "name": "硅谷电影🎬",
+  //     "url": `${BASEURL}/movie`
+  //   },
+  //   {
+  //     "type": "view",
+  //     "name": "语音识别🎤",
+  //     "url": `${BASEURL}/search`
+  //   },
+  //   {
+  //     "name": "戳我💋",
+  //     "sub_button": [
+  //       {
+  //         "type": "view",
+  //         "name": "官网☀",
+  //         "url": "http://www.atguigu.com"
+  //       },
+  //       {
+  //         "type": "click",
+  //         "name": "帮助🙏",
+  //         "key": "help"
+  //       }
+  //     ]
+  //   }
+  // ]
+
   "button": [
     {
-      "type": "view",
-      "name": "硅谷电影🎬",
-      "url": `${BASEURL}/movie`
+      "type": "click",
+      "name": "今日歌曲",
+      "key": "V1001_TODAY_MUSIC"
     },
     {
-      "type": "view",
-      "name": "语音识别🎤",
-      "url": `${BASEURL}/search`
-    },
-    {
-      "name": "戳我💋",
+      "name": "菜单",
       "sub_button": [
         {
           "type": "view",
-          "name": "官网☀",
-          "url": "http://www.atguigu.com"
+          "name": "搜索",
+          "url": "http://www.soso.com/"
+        },
+        {
+          "type": "miniprogram",
+          "name": "wxa",
+          "url": "http://mp.weixin.qq.com",
+          "appid": "wx286b93c14bbf93aa",
+          "pagepath": "pages/lunar/index"
         },
         {
           "type": "click",
-          "name": "帮助🙏",
-          "key": "help"
-        }
-      ]
-    }
-  ]
+          "name": "赞一下我们",
+          "key": "V1001_GOOD"
+        }]
+    }]
 }
