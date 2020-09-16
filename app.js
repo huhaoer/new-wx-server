52bcf7d5c4d91f85c94ab2994201d02d16469973
layoung <184592775@qq.com>
2020-09-16 12:31:54 +0800

    'menu'

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var indexRouter = require('./routes/index');
 var app = express();
 const { fetchAccessToken } = require('./utils/access_token')
 const { fetchJsapiTicket } = require('./utils/jsapi_ticket')
+const { createMenu, deleteMenu } = require('./utils/wx_menu')
+const menu = require('./wechat/menu')
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,27 +18,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.get('/accesstoken', async (req, res) => {
-    // 测试获取access_token
-    try {
-        const accessToken = await fetchAccessToken()
-        res.send(accessToken)
-    } catch (error) {
-        console.log(error, '获取access_token的错误');
-        res.send('获取access_token失败')
-    }
-})
 
-app.get('/jsapiticket', async (req, res) => {
-    // 测试获取access_token
-    try {
-        const jsapi_ticket = await fetchJsapiTicket()
-        res.send(jsapi_ticket)
-    } catch (error) {
-        console.log(error, '获取jsapi_ticket的错误');
-        res.send('jsapi_ticket失败')
-    }
-})
-
+// 删除菜单
+deleteMenu();
+// 创建菜单
+createMenu(menu);
 
 module.exports = app;
--- utils/wx_menu.js
+++ utils/wx_menu.js
@@ -25,13 +25,15 @@ async function deleteMenu() {
  * @param {*} menuList 菜单数据
  */
 async function createMenu(menuList) {
+    console.log(menuList,'menuList');
     try {
         // 获取access_token
         const access_token = await fetchAccessToken();
         //定义请求地址
         const url = _getCreateMenu(access_token.access_token);
         //发送请求
-        const result = await axios.get(url);
+        const result = await axios.post(url, menuList);
+        console.log(result,'创建菜单的结果');
         return result
     } catch (error) {
         return Promise.reject('createMenu方法出了问题：' + error);
