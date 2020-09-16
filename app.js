ae4a6b3687ccc81fc0d53ad2119b8c1991c65853
layoung <184592775@qq.com>
2020-09-16 13:02:09 +0800

    'menu'

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ var indexRouter = require('./routes/index');
 var app = express();
 const { fetchAccessToken } = require('./utils/access_token')
 const { fetchJsapiTicket } = require('./utils/jsapi_ticket')
-const { createMenu, deleteMenu } = require('./utils/wx_menu')
-const menu = require('./wechat/menu')
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -18,15 +16,27 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-
-// 删除菜单
-// deleteMenu();
-// // // 创建菜单
-// createMenu(menu);
-
-app.post('/menu', async (req, res) => {
-    const result = await axios.post('https://api.weixin.qq.com/cgi-bin/menu/create?access_token=37_5H3rlkMTQaFfiYdMkXYqO-CdjEPasJDo31kvRqu2OAhq7W2UsnazUc02zH58qqfe4oQ4jPAykQn_JQ8vyYqY9s1iugIi5_Xt064a8VutscPliJ6s7wZyO5t2o72jx-Pdjd9kKo18_E3tuEooABQdAGAVWL',menu)
-    res.send(result)
+app.get('/accesstoken', async (req, res) => {
+    // 测试获取access_token
+    try {
+        const accessToken = await fetchAccessToken()
+        res.send(accessToken)
+    } catch (error) {
+        console.log(error, '获取access_token的错误');
+        res.send('获取access_token失败')
+    }
 })
+
+app.get('/jsapiticket', async (req, res) => {
+    // 测试获取access_token
+    try {
+        const jsapi_ticket = await fetchJsapiTicket()
+        res.send(jsapi_ticket)
+    } catch (error) {
+        console.log(error, '获取jsapi_ticket的错误');
+        res.send('jsapi_ticket失败')
+    }
+})
+
 
 module.exports = app;
