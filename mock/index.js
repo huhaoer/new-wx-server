dd04aa69fb69a942747dd3056a573ae7c5d50f4d
layoung <184592775@qq.com>
2020-09-18 20:52:40 +0800

    'userdata'

--- mock/index.js
+++ mock/index.js
@@ -7,25 +7,25 @@
         title: '从邪恶中拯救我',
         description: '泰国发生震惊人心的绑架事件，完成最后杀人任务的杀手仁南（黄政民饰演），在得知这事件与自己有关之后，随即前往泰国，并与助手开始追查这起事件。另一方面，得知自己兄弟被仁南杀害的雷（李政宰饰演），为了复仇，也前往了泰国。惨烈的暗杀者与无情的追击者，展开了一场没有底线的猛烈追击。',
         picUrl: `https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2612814747.jpg`,
-        url: 'http:..www.baidu.com'
+        url: 'http://www.baidu.com'
      },
      {
          title: '我想结束这一切',
          description: '剧本改编自伊恩·里德同名小说，小说审视了心灵的脆弱和孤独的极限。杰克原本带女友回家见父母，女友一路却在想着“结束这一切”，在杰克改变原定路线后，一切都朝着失控方向发展。',
          picUrl: 'https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2619279677.jpg',
-         url: 'http:..www.baidu.com'
+         url: 'http://www.baidu.com'
      },
      {
         title: '神弃之地',
         description: '该片是一部中西部哥特风电影，安东尼奥·坎帕斯(《克里斯汀》)执导，讲述俄亥俄州偏远之地Knockemstiff，男主角Willard Russell绝望地想拯救濒死的妻子，不惜屈服于牺牲，而他的儿子(霍兰德)正从一个在学校被虐待的孩子成长为知道何时出手的男人，角色包括一对连环杀手夫妇、一个考验人的信仰的传教士、一个腐败的当地警长(斯坦)。时间从二战末期到1960年代，地点从南俄亥俄州乡村到西维吉尼亚。',
         picUrl: 'https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2619322729.jpg',
-        url: 'http:..www.baidu.com'
+        url: 'http://www.baidu.com'
     },
     {
         title: '意大利制造',
         description: '一位画家（连姆·尼森 Liam Neeson 饰），带着他关系疏远的儿子（米切尔·理查德森 Micheál Richardson 饰），两人从伦敦返回义大利，想出售已故妻子多年前一起生活过的房子。不过这栋房子年久失修，客厅墙壁还有一大片难以理解的彩绘，导致完全没人想买下这栋房子。两人只好试图与当地居民，一同运用手边仅有的工具，开始修缮这栋老旧房屋。同时，这对父子也没想到，当他们放下繁忙生活，来到义大利托斯卡纳乡间，似乎也开始找到属于他们的未来……',
         picUrl: 'https://img2.doubanio.com/view/photo/s_ratio_poster/public/p2612140333.jpg',
-        url: 'http:..www.baidu.com'
+        url: 'http://www.baidu.com'
     },
     {
         title: '怪胎',
@@ -36,7 +36,7 @@
         　　陈柏青是一名严重神经性强迫症患者，有非常严重的洁癖。每月的15号是柏青唯一一天出门採买的日子，就在某个命运的15号，他遇见了与他「同病相怜」的另一个怪胎—陈静。
         　　原本以为自己要永远孤单活在这世界上的两人，老天爷让他们相遇，发展出一尘不染的爱情，彷彿拥有了彼此就不再害怕被世界遗弃。然而这段看似命中注定的关係，却在柏青的强迫症突然消失后，渐渐的变了调…爱情的承诺，真的能永保新鲜吗？`,
         picUrl: 'https://img9.doubanio.com/view/photo/s_ratio_poster/public/p2613241995.jpg',
-        url: 'http:..www.baidu.com'
+        url: 'http://www.baidu.com'
     },
 
  ]
