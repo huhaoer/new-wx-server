c485e864965fd3e97298b26437d4dde96de87bad
layoung <184592775@qq.com>
2020-09-18 19:25:41 +0800

    'userdata'

--- utils/user_message.js
+++ utils/user_message.js
@@ -8,17 +8,21 @@
  * @param {*} req 用户发送数据的post请求的请求头参数
  */
 async function getUserDataAsync(req) {
-    let xmlData = '';
-    req.on('data', data => {
-        console.log(data,'data================');
-        //当流式数据传递过来时，会触发当前事件，会将数据注入到回调函数中
-        // console.log(data.toString());
-        //读取的数据是buffer，需要将其转化成字符串
-        xmlData += data.toString();
-        console.log(xmlData,'func       ==============     xmlData');
-    }).on('end', () => {
-        //当数据接受完毕时，会触发当前
-        return xmlData
+    return new Promise((resolve, reject) => {
+        let xmlData = '';
+        req
+            .on('data', data => {
+                console.log(data,'data=========');
+                //当流式数据传递过来时，会触发当前事件，会将数据注入到回调函数中
+                // console.log(data.toString());
+                //读取的数据是buffer，需要将其转化成字符串
+                xmlData += data.toString();
+                console.log(xmlData,'xmlData?????');
+            })
+            .on('end', () => {
+                //当数据接受完毕时，会触发当前
+                resolve(xmlData);
+            })
     })
 }
 
